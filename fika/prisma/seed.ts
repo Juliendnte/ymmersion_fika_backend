749e078e442e18574989e02542c4fe2dbd3e4e9c
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2025-02-20 16:11:05 +0100

    perf (seed): amélioration des fake datas

--- fika/prisma/seed.ts
+++ fika/prisma/seed.ts
@@ -1,4 +1,4 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
@@ -7,7 +7,7 @@ async function main() {
     console.log("Seeding database...");
 
     const roleAdmin = await prisma.role.upsert({
-        where: {role: "admin"},
+        where: { role: "admin" },
         update: {},
         create: {
             role: "admin",
@@ -15,7 +15,7 @@ async function main() {
     });
 
     const roleUser = await prisma.role.upsert({
-        where: {role: "Member"},
+        where: { role: "Member" },
         update: {},
         create: {
             role: "Member",
@@ -26,7 +26,7 @@ async function main() {
     const hashedPasswordMember = await bcrypt.hash('memberPassword', 10)
 
     const userAdmin = await prisma.user.upsert({
-        where: {email: "admin@example.com"},
+        where: { email: "admin@example.com" },
         update: {},
         create: {
             name: "Admin User",
@@ -37,7 +37,7 @@ async function main() {
     });
 
     const userMember = await prisma.user.upsert({
-        where: {email: "user@example.com"},
+        where: { email: "user@example.com" },
         update: {},
         create: {
             name: "Normal User",
@@ -66,58 +66,73 @@ async function main() {
     }
 
     const categories = [
-        {name: "Plats Chauds Y-novants"},
-        {name: "Les salades Y-novante"},
-        {name: "Y-Snack"},
-        {name: "Y-Dessert"},
+        { name: "Plats Chauds Y-novants" },
+        { name: "Les salades Y-novante" },
+        { name: "Y-Snack" },
+        { name: "Y-Dessert" },
+        { name: "Y-Boisson" },
     ];
 
     for (const category of categories) {
         await prisma.category.upsert({
-            where: {name: category.name},
+            where: { name: category.name },
             update: {},
             create: category,
         });
     }
 
     const types = [
-        {name: "Plats"},
-        {name: "Boissons"},
-        {name: "Desserts"},
-        {name: "Snacks"},
+        { name: "Plats" },
+        { name: "Boissons" },
+        { name: "Desserts" },
+        { name: "Snacks" },
     ];
 
     for (const type of types) {
         await prisma.type.upsert({
-            where: {name: type.name},
+            where: { name: type.name },
             update: {},
             create: type,
         });
     }
 
     const ingredients = [
-        {name: "Pâte Rigatoni", quantity: 500, unit: "g"},
-        {name: "Sauce tomate", quantity: 200, unit: "ml"},
-        {name: "Fromage râpé", quantity: 100, unit: "g"},
-        {name: "Bœuf haché", quantity: 300, unit: "g"},
-        {name: "Crème liquide", quantity: 150, unit: "ml"},
-        {name: "Chocolat noir", quantity: 200, unit: "g"},
-        {name: "Œufs", quantity: 3, unit: "pcs"},
-        {name: "Spéculos", quantity: 80, unit: "g"},
-        {name: "Kinder Bueno", quantity: 2, unit: "bars"},
-        {name: "Tranche de pain", quantity: 2, unit: "pcs"},
-        {name: "Laitue", quantity: 1, unit: "head"},
-        {name: "Crevettes", quantity: 150, unit: "g"},
-        {name: "Poulet", quantity: 200, unit: "g"},
-        {name: "Semoule", quantity: 150, unit: "g"},
-        {name: "Légumes frais", quantity: 200, unit: "g"},
-        {name: "Truite", quantity: 200, unit: "g"},
-        {name: "Riz", quantity: 150, unit: "g"},
+        { name: "Pâte Rigatoni", quantity: 500, unit: "g" },
+        { name: "Sauce tomate", quantity: 200, unit: "ml" },
+        { name: "Fromage râpé", quantity: 100, unit: "g" },
+        { name: "Bœuf haché", quantity: 300, unit: "g" },
+        { name: "Crème liquide", quantity: 150, unit: "ml" },
+        { name: "Chocolat noir", quantity: 200, unit: "g" },
+        { name: "Œufs", quantity: 3, unit: "pcs" },
+        { name: "Spéculos", quantity: 80, unit: "g" },
+        { name: "Kinder Bueno", quantity: 2, unit: "bars" },
+        { name: "Tranche de pain", quantity: 2, unit: "pcs" },
+        { name: "Laitue", quantity: 1, unit: "head" },
+        { name: "Crevettes", quantity: 150, unit: "g" },
+        { name: "Poulet", quantity: 200, unit: "g" },
+        { name: "Semoule", quantity: 150, unit: "g" },
+        { name: "Légumes frais", quantity: 200, unit: "g" },
+        { name: "Truite", quantity: 200, unit: "g" },
+        { name: "Riz", quantity: 150, unit: "g" },
+        { name: "Tomate", quantity: 20, unit: "pcs" },
+        { name: "Chocolat en poudre", quantity: 200, unit: "g" },
+        { name: "Fruit rouges", quantity: 1000, unit: "g" },
+        { name: "Lait", quantity: 2000, unit: "ml" },
+        { name: "Crème", quantity: 200, unit: "ml" },
+        { name: "Eau froide", quantity: 500000, unit: "ml" },
+        { name: "Thé a la peche", quantity: 200, unit: "g" },
+        { name: "Peche", quantity: 15, unit: "pcs" },
+        { name: "Eau chaude", quantity: 20000, unit: "ml" },
+        { name: "Thé a la menthe", quantity: 300, unit: "g" },
+        { name: "Menthe", quantity: 10, unit: "g" },
+        { name: "Poire", quantity: 10, unit: "pcs" },
+        { name: "Spaghetti", quantity: 1500, unit: "g" },
+        { name: "Sauce thai", quantity: 100, unit: "ml" },
     ];
 
     for (const ingredient of ingredients) {
         await prisma.ingredient.upsert({
-            where: {name: ingredient.name},
+            where: { name: ingredient.name },
             update: {},
             create: ingredient,
         })
@@ -142,7 +157,7 @@ async function main() {
             price: 6.99,
             promotion: 20, // Réduction de 20 %
             idCategory: 4,
-            idType: 4,
+            idType: 3,
             imagePath: "/produits/IMG_7058.jpg"
         },
         {
@@ -152,7 +167,7 @@ async function main() {
             price: 10.99,
             available: true,
             isPlatDuJour: true,
-            idType: 1,
+            idType: 3,
             imagePath: "/produits/IMG_7062.jpg"
         },
         {
@@ -162,7 +177,7 @@ async function main() {
             price: 8.99,
             available: true,
             isPlatDuJour: true,
-            idType: 1,
+            idType: 3,
             imagePath: "/produits/IMG_7065.jpg"
         },
         {
@@ -211,7 +226,7 @@ async function main() {
             price: 2.99,
             promotion: 20, // Réduction de 20%
             idCategory: 4,
-            idType: 3,
+            idType: 1,
             imagePath: "/produits/IMG_7080.jpg"
         },
         {
@@ -233,14 +248,59 @@ async function main() {
             available: true,
             promotion: 25, // Réduction de 25%
             idCategory: 4,
-            idType: 4,
+            idType: 3,
             imagePath: "/produits/IMG_7073.jpg"
+        },
+
+        {
+            name: "Chocolat chaud",
+            uidUser: userMember.uid,
+            description: "Une boisson chocolatée pour se réchauffer un peu cet hiver",
+            price: 1.30,
+            available: true,
+            promotion: 0, // Réduction de 25%
+            idCategory: 5,
+            idType: 2,
+            imagePath: "/produits/chocolat_chaud.jpg"
+        },
+        {
+            name: "Smoothie fruits rouges",
+            uidUser: userMember.uid,
+            description: "Une boisson fruitée pour aigailler votre journée",
+            price: 2.20,
+            available: true,
+            promotion: 0, // Réduction de 25%
+            idCategory: 5,
+            idType: 2,
+            imagePath: "/produits/smoothie_fruit_rouges.jpg"
+        },
+        {
+            name: "Thé glacé peche",
+            uidUser: userMember.uid,
+            description: "Une boisson fruitée pour aigailler votre journée",
+            price: 1.99,
+            available: true,
+            promotion: 0, // Réduction de 25%
+            idCategory: 5,
+            idType: 2,
+            imagePath: "/produits/the_glace_peche.jpg"
+        },
+        {
+            name: "Thé à la menthe",
+            uidUser: userMember.uid,
+            description: "Petit thé marocain pour Kheir le GOAT",
+            price: 0.99,
+            available: true,
+            promotion: 10,
+            idCategory: 5,
+            idType: 2,
+            imagePath: "/produits/the_menthe.jpg"
         },
     ];
 
     for (const produit of produitsAvecDescriptions) {
         await prisma.produit.upsert({
-            where: {name: produit.name},
+            where: { name: produit.name },
             update: {},
             create: produit,
         });
@@ -250,76 +310,111 @@ async function main() {
         {
             produitName: "Pates Rigatoni",
             ingredients: [
-                {name: "Pâte Rigatoni", quantity: 200},
-                {name: "Sauce tomate", quantity: 100},
-                {name: "Fromage râpé", quantity: 50},
+                { name: "Pâte Rigatoni", quantity: 200 },
+                { name: "Sauce tomate", quantity: 100 },
+                { name: "Fromage râpé", quantity: 50 },
             ],
         },
         {
             produitName: "Fondant au chocolat",
             ingredients: [
-                {name: "Chocolat noir", quantity: 120},
-                {name: "Œufs", quantity: 2},
-                {name: "Crème liquide", quantity: 50},
+                { name: "Chocolat noir", quantity: 120 },
+                { name: "Œufs", quantity: 2 },
+                { name: "Crème liquide", quantity: 50 },
             ],
         },
         {
             produitName: "Tiramisu au poire spéculos",
             ingredients: [
-                {name: "Spéculos", quantity: 50},
-                {name: "Crème liquide", quantity: 100},
+                { name: "Spéculos", quantity: 50 },
+                { name: "Crème liquide", quantity: 100 },
+                { name: "Poire", quantity: 2 }
             ],
         },
         {
             produitName: "Tiramisu kinder bueno",
             ingredients: [
-                {name: "Kinder Bueno", quantity: 1},
-                {name: "Crème liquide", quantity: 100},
+                { name: "Kinder Bueno", quantity: 1 },
+                { name: "Crème liquide", quantity: 100 },
             ],
         },
         {
             produitName: "Burger Chicken",
             ingredients: [
-                {name: "Poulet", quantity: 150},
-                {name: "Tranche de pain", quantity: 2},
-                {name: "Laitue", quantity: 50},
+                { name: "Poulet", quantity: 150 },
+                { name: "Tranche de pain", quantity: 2 },
+                { name: "Laitue", quantity: 50 },
+                { name: "Tomate", quantity: 1 },
+
             ],
         },
         {
             produitName: "Pates au crevettes",
             ingredients: [
-                {name: "Crevettes", quantity: 100},
-                {name: "Pâte Rigatoni", quantity: 200},
-                {name: "Sauce tomate", quantity: 80},
+                { name: "Crevettes", quantity: 100 },
+                { name: "Spaghetti", quantity: 200 },
+                { name: "Sauce thai", quantity: 80 },
             ],
         },
         {
             produitName: "Taboulet",
             ingredients: [
-                {name: "Semoule", quantity: 100},
-                {name: "Légumes frais", quantity: 100},
+                { name: "Semoule", quantity: 100 },
+                { name: "Légumes frais", quantity: 100 },
             ],
         },
         {
             produitName: "Pokéball Truite",
             ingredients: [
-                {name: "Truite", quantity: 100},
-                {name: "Riz", quantity: 100},
-                {name: "Légumes frais", quantity: 50},
+                { name: "Truite", quantity: 100 },
+                { name: "Riz", quantity: 100 },
+                { name: "Légumes frais", quantity: 50 },
             ],
         },
         {
             produitName: "Salade César",
             ingredients: [
-                {name: "Poulet", quantity: 100},
-                {name: "Laitue", quantity: 50},
+                { name: "Poulet", quantity: 100 },
+                { name: "Laitue", quantity: 50 },
+                { name: "Tomate", quantity: 1 },
+            ],
+        },
+        {
+            produitName: "Chocolat chaud",
+            ingredients: [
+                { name: "Chocolat en poudre", quantity: 50 },
+                { name: "Lait", quantity: 100 },
+            ],
+        },
+        {
+            produitName: "Smoothie fruits rouges",
+            ingredients: [
+                { name: "Fruit rouges", quantity: 100 },
+                { name: "Lait", quantity: 100 },
+                { name: "Crème", quantity: 50 },
+            ],
+        },
+        {
+            produitName: "Thé glacé peche",
+            ingredients: [
+                { name: "Eau froide", quantity: 100 },
+                { name: "Thé a la peche", quantity: 20 },
+                { name: "Peche", quantity: 2 },
+            ],
+        },
+        {
+            produitName: "Thé à la menthe",
+            ingredients: [
+                { name: "Eau chaude", quantity: 100 },
+                { name: "Thé a la menthe", quantity: 20 },
+                { name: "Menthe", quantity: 2 },
             ],
         },
     ];
 
     for (const relation of produitIngredientsRelations) {
         const produit = await prisma.produit.findUnique({
-            where: {name: relation.produitName},
+            where: { name: relation.produitName },
         });
 
         if (!produit) {
@@ -329,7 +424,7 @@ async function main() {
 
         for (const ingredient of relation.ingredients) {
             const ingredientRecord = await prisma.ingredient.findUnique({
-                where: {name: ingredient.name},
+                where: { name: ingredient.name },
             });
 
             if (!ingredientRecord) {
--- fika/src/module/order/order.module.ts
+++ fika/src/module/order/order.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import {OrderController} from "src/module/order/order.controller";
+import {OrderService} from "src/module/order/order.service";
 
-@Module({})
+@Module({
+    controllers: [OrderController],
+    providers: [OrderService],
+})
 export class OrderModule {}
